99853a762877956564f0b29618f7131d90f9a1ab
Nellark <nelakulati@gmail.com>
2025-03-14 11:30:49 +0200

    added home page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')  // Add Flowbite plugin
+  ],
+};
+
