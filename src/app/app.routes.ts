d54cc6cd7222931eaa390652aefa9e93bb370308
Nellark <nelakulati@gmail.com>
2025-03-18 11:43:31 +0200

    linked my projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 
 export const routes: Routes = [
 
     {path: '', title: 'Home', component: HomeComponent},
     {path: 'navbar', title: 'Navbar', component: NavbarComponent},
-    {path: 'footer', title: 'Footer', component: FooterComponent}
+ 
 ];
